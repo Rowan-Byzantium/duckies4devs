30e627f9a01c319e1bb4f3dc28038b3042585318
Rowan Byzantium <verte735@gmail.com>
2023-05-09 11:38:08 +0200

    adding one css stylesheet, html class & pics, beginning javascrip

---
+++ script.js
@@ -0,0 +1,8 @@
+let buttonLeft = document.querySelector(".page-pics .button-left")
+console.log(buttonLeft);
+
+buttonLeft.addEventListener("click", function(){
+
+})
+
+const pics = document.querySelectorAll(".thumbs")
