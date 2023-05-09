849e2bee1f2d181fbce45956525cb69fa9d2131a
Rowan Byzantium <verte735@gmail.com>
2023-05-09 12:20:10 +0200

    function to change image while hovering

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-let buttonLeft = document.querySelector(".page-pics .button-left")
-console.log(buttonLeft);
+// let buttonLeft = document.querySelector(".page-pics .button-left")
+// console.log(buttonLeft);
 
-buttonLeft.addEventListener("click", function(){
+// buttonLeft.addEventListener("click", function(){
+
+// })
+
+const pics = document.querySelectorAll(".thumbs .thumbs-img")
+console.log(pics);
+//select all thumbs pics in bigger size
+const frame = document.querySelector(".pictures-img")
+console.log(frame);
+//select the place where the bigger pic will be display
+
+pics.forEach(function (pic) {
+    //to apply for each pictures
+
+    pic.addEventListener("mouseover", function (e) {
+        frame.setAttribute("src", this.getAttribute("srcset"));
+    })
+    //replace the source image of the frame by the srcset of the image hover
 
 })
 
-const pics = document.querySelectorAll(".thumbs")
