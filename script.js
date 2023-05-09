2a7226f5438552411a7711b4952905c88a83407e
Rowan Byzantium <verte735@gmail.com>
2023-05-09 14:15:28 +0200

    fonctions pour faire défiler la liste sur mobile (ne fonctionne pas)

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-// let buttonLeft = document.querySelector(".page-pics .button-left")
-// console.log(buttonLeft);
-
-// buttonLeft.addEventListener("click", function(){
-
-// })
-
-const pics = document.querySelectorAll(".thumbs .thumbs-img")
-console.log(pics);
+const pics = document.querySelectorAll(".thumbs .thumbs-img");
 //select all thumbs pics in bigger size
-const frame = document.querySelector(".pictures-img")
-console.log(frame);
+const frame = document.querySelector(".pictures-img");
 //select the place where the bigger pic will be display
 
 pics.forEach(function (pic) {
     //to apply for each pictures
-
+    
     pic.addEventListener("mouseover", function (e) {
         frame.setAttribute("src", this.getAttribute("srcset"));
     })
     //replace the source image of the frame by the srcset of the image hover
-
+    
 })
 
+
+const btnLft = document.querySelector(".page-pics .button-left");
+const btnRgt = document.querySelector('.page-pics .button-right');
+
+btnLft.addEventListener('click', function(e) {
+    const ulThumbs = document.querySelector('#thumb-list');
+    ulThumbs.appendChild(ulThumbs.firstElementChild);
+});
+
+btnRgt.addEventListener("click", function(e){
+    const ThumbList = document.querySelector('#thumb-list');
+    ThumbList.appendChild(ThumbList.lastElementChild)
+})
